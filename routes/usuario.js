5cd67631e9aee90b56578262d809228435a788b2
Raphael Freitas <raphaelfreitasadm@gmail.com>
2019-06-12 15:22:31 -0300

    logout

--- routes/usuario.js
+++ routes/usuario.js
@@ -82,4 +82,9 @@ router.post("/login", (req,res,next)=>{
         failureFlash:true
     })(req,res,next)
 })
+router.get("/logout", (req,res)=>{
+    req.logout()
+    req.flash("success_msg","Deslogado com sucesso")
+    res.redirect("/")
+})
 module.exports= router
