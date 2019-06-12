0701753f528cd5780c1c0d7ade8f43310e586024
Raphael Freitas <raphaelfreitasadm@gmail.com>
2019-06-12 17:41:08 -0300

    Conexao Heroku

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-module.exports={mongoURI:"mongodb+srv://raphaelfreitas:<rph9141#>@cluster0-aiubv.mongodb.net/test"}
+module.exports={mongoURI:"mongodb+srv://raphaelfreitas:<rph9141#>@cluster0-aiubv.mongodb.net/test?retryWrites=true&w=majority"}
 }else{
 module.exports={mongoURI:"mongodb://localhost/blogapp"}
 }
