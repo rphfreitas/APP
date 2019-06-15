be58f8f331208dd5749278a0f5e740d69543b05a
Raphael Freitas <raphaelfreitasadm@gmail.com>
2019-06-14 23:55:16 -0300

    mudança na index

--- routes/usuario.js
+++ routes/usuario.js
@@ -39,7 +39,8 @@ router.post("/registro",(req,res)=>{
                 const novoUsuario = new Usuario({
                     nome: req.body.nome,
                     email:req.body.email,
-                    senha:req.body.senha
+                    senha:req.body.senha,
+                    eAdmin:1
                     
                 })
 
